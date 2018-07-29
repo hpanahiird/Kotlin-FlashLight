593dcc9123a9c5cebddd50ecc3cf7be751f7279f
Hossein Panahi <h.panahi.ird@gmail.com>
2018-07-29 14:09:30 +0430

    granting camera permission in app starting for api 23 and above.

--- app/src/main/java/ir/hpanahi/flashlight/MainActivity.kt
+++ app/src/main/java/ir/hpanahi/flashlight/MainActivity.kt
@@ -1,12 +1,39 @@
 package ir.hpanahi.flashlight
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_CAMERA_PERMISSION = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupPermission()
     }
+
+    private fun setupPermission(){
+        val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkSelfPermission(Manifest.permission.CAMERA)
+        } else {
+            PackageManager.PERMISSION_GRANTED
+            TODO("VERSION.SDK_INT < M")
+        }
+
+        Log.d("permissions","$permission")
+        val permissions = Array(1){Manifest.permission.CAMERA}
+        if (permission!=PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(permissions,REQUEST_CODE_CAMERA_PERMISSION)
+            }
+        }
+    }
+
+
 }
