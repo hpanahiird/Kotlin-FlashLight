aa29a1add06d10a4f98b6c31ba78db1dd909890d
Hossein Panahi <h.panahi.ird@gmail.com>
2018-07-29 13:07:12 +0430

    initial commit

---
+++ app/src/main/java/ir/hpanahi/flashlight/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.hpanahi.flashlight
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
